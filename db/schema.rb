5b6eff4ac3ae3b84690820d4973bd038d04d8b95
Gustavo Vieira <gustavo.vieira@betalearning.com.br>
2022-06-08 01:21:32 -0300

    add username and email to user

--- db/schema.rb
+++ db/schema.rb
@@ -22,6 +22,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_08_034633) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
